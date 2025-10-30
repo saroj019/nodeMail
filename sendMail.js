77c69dc8ea8f7989d09cf5f0d8d6eeea46e24ef1
saroj019 <156309823+saroj019@users.noreply.github.com>
2025-10-30 15:29:55 +0530

    Update email sender and recipient placeholders

--- sendMail.js
+++ sendMail.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport(
     host : 'smtp.gmail.com',
     port : 465,
     auth : {
-      user : '1911saroj@gmail.com',
+      user : 'Enter your  sender email',
       pass : 'const pass = process.env.pass;'
       
     }
@@ -25,7 +25,7 @@ function sendMail(to, subject, text) {
 }
 console.log('mail send successfully');
 
-sendMail("1911saroj@gmail.com",
+sendMail("Enter the mail where you have to send the mail",
   "Hello this is test mail",
   "<h1>Hello this is test mail</h1><p>Keep Quit While Eating..!</p><img src='https://imgs.search.brave.com/2GVw28EFzOaW6SVvwGJraTqB7nnmkUemb9tosBuqVJY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi93b21h/bi1lYXRpbmctY29v/a2llLTEyNTg3ODcz/LmpwZw'/> <p>Are you Hungery");
 
