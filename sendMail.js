0185f36bfa0604550d55bc732da8f00178d81154
Saroj <1911saroj@gmail.com>
2025-10-30 15:28:04 +0530

    first commit

---
+++ sendMail.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const transporter = nodemailer.createTransport(
+  {
+    secure : true,
+    host : 'smtp.gmail.com',
+    port : 465,
+    auth : {
+      user : '1911saroj@gmail.com',
+      pass : 'const pass = process.env.pass;'
+      
+    }
+  }
+);
+function sendMail(to, subject, text) {
+  transporter.sendMail(
+    {
+      to : to,
+      subject : subject,
+      html : text,
+    }
+  );
+}
+console.log('mail send successfully');
+
+sendMail("1911saroj@gmail.com",
+  "Hello this is test mail",
+  "<h1>Hello this is test mail</h1><p>Keep Quit While Eating..!</p><img src='https://imgs.search.brave.com/2GVw28EFzOaW6SVvwGJraTqB7nnmkUemb9tosBuqVJY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90aHVt/YnMuZHJlYW1zdGlt/ZS5jb20vYi93b21h/bi1lYXRpbmctY29v/a2llLTEyNTg3ODcz/LmpwZw'/> <p>Are you Hungery");
+
+
